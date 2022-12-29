ea84228cf71d9d359059015e2b1d2f01a6e0b257
Rafael Gayoso <gayoso0597@gmail.com>
2022-12-29 18:58:18 +0100

    Project Initialization

---
+++ src/test/java/com/gsoft/studentsmanagement/StudentsmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsoft.studentsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
